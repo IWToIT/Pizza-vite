8729dbd56313b3e27cb3a1a516dccf76aca1b7a6
IWToIT <matylitski.egor.99@gmail.com>
2024-10-08 22:07:41 +0300

    fix: aliases work!

--- src/utils/consts.js
+++
@@ -1,3 +0,0 @@
-export const pizzaData = 'https://6474f3f57de100807b1bfd08.mockapi.io/pizzas/';
-export const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
-export const typeNames = ['тонкое', 'традиционное'];
